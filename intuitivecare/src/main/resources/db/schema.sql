4910ebd7613ae59e125e7dc6bb5066061bc20c78
aldacelio <aldacelio@alu.ufc.br>
2025-03-31 23:37:24 -0300

    fix: correcting folder name and table columns

--- intuitivecare/src/main/resources/db/schema.sql
+++ intuitivecare/src/main/resources/db/schema.sql
@@ -14,21 +14,23 @@ CREATE TABLE IF NOT EXISTS operadoras (
     ddd VARCHAR(5),
     telefone VARCHAR(20),
     fax VARCHAR(20),
-    email VARCHAR(100),
+    endereco_eletronico VARCHAR(100),
     representante VARCHAR(100),
     cargo_representante VARCHAR(100),
+    regiao_de_comercializacao VARCHAR(100),
     data_registro_ans DATE
 );
 
 CREATE TABLE IF NOT EXISTS demonstracoes_contabeis (
     id SERIAL PRIMARY KEY,
     data DATE NOT NULL,
-    registro_ans VARCHAR(20) NOT NULL,
-    codigo_conta VARCHAR(20) NOT NULL,
+    reg_ans VARCHAR(20) NOT NULL,
+    cd_conta_contabil VARCHAR(20) NOT NULL,
     descricao VARCHAR(255) NOT NULL,
-    valor_saldo_final NUMERIC(15,2) NOT NULL,
-    FOREIGN KEY (registro_ans) REFERENCES operadoras(registro_ans)
+    vl_saldo_inicial NUMERIC(15,2),
+    vl_saldo_final NUMERIC(15,2)
 );
 
 CREATE INDEX IF NOT EXISTS idx_dc_data ON demonstracoes_contabeis(data);
 CREATE INDEX IF NOT EXISTS idx_dc_descricao ON demonstracoes_contabeis(descricao);
+CREATE INDEX IF NOT EXISTS idx_dc_reg_ans ON demonstracoes_contabeis(reg_ans);
